90ae709dc7b01fb59e29deea7bcd687bbb040926
fatredwombat1-beep <fatredwombat1@gmail.com>
2026-06-05 19:35:02 +1000

    Update get_temp.sh

--- scripts/get_temp.sh
+++ scripts/get_temp.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# Description: Fetches ROC temperature from LSI 9300-8i
-# Requires: storcli
+# Fetch temperature using the storcli symlink we created
+TEMP=$(/usr/local/sbin/storcli /c0 show all | grep -i "ROC temperature" | awk '{print $4}')
 
-# Run storcli, target controller 0, find the temperature line, and extract the integer
-TEMP=$(/path/to/storcli /c0 show all | grep -i "ROC temperature" | awk '{print $4}')
-
-# Output just the number (e.g., 55) for Unraid to parse
-echo $TEMP
+# Save to RAM-disk file for the GUI to read instantly
+echo $TEMP > /tmp/lsi_temp.txt
