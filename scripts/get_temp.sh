9980173723d872d441657c130c758109d8da0009
fatredwombat1-beep <fatredwombat1@gmail.com>
2026-06-05 19:53:00 +1000

    Update get_temp.sh

--- scripts/get_temp.sh
+++ scripts/get_temp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Fetch temperature using the storcli symlink we created
-TEMP=$(/usr/local/sbin/storcli /c0 show all | grep -i "ROC temperature" | awk '{print $4}')
+# Fetch temperature and isolate the number after the equals sign
+TEMP=$(/usr/local/sbin/storcli /c0 show all | grep -i "ROC temperature" | awk -F'=' '{print $2}' | tr -d ' ')
 
 # Save to RAM-disk file for the GUI to read instantly
 echo $TEMP > /tmp/lsi_temp.txt
